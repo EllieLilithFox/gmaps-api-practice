43534f2c480fb4ae6128d2265eeccadbf924cec3
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-01 15:46:17 -0700

    add simple map
    co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
 
-$(document).ready(function() {
+(function(exports) {
+  "use strict";
 
-});
+  function initMap() {
+    exports.map = new google.maps.Map(document.getElementById("map"), {
+      center: {
+        lat: -34.397,
+        lng: 150.644
+      },
+      zoom: 8
+    });
+  }
+
+  exports.initMap = initMap;
+})((this.window = this.window || {}));
